3ce368f8523532dd74f83badd88172a9916f10fd
meneerdennis <158499471+meneerdennis@users.noreply.github.com>
2024-02-08 16:22:09 +0100

    Update Les 3 - opdrachten.py

--- Les 3 - opdrachten.py
+++ Les 3 - opdrachten.py
@@ -1,1 +1,73 @@
-# Maak hier de opdrachten van Les 3!
+# Opgave 1:
+# Schrijf een programma dat de voornaam van de gebruiker opvraagt met de functie input. Vervolgens wordt de gebruiker gegroet met 'Hallo, [en hier komt de voornaam van de gebruiker]!'.
+# Hint: denk aan concatenation!
+
+
+# Opgave 2:
+# Schrijf een programma dat de gebruiker vraagt om een zelfstandig naamwoord (bv. boom, huis, kast), een bijvoeglijk naamwoord (bv. groot, vies, vervelend) en een infinitief van een werkwoord (bv. lopen, schrijven, eten).
+# Vul de ingevoerde vervolgens in, in de onderstaande zin en zet het resultaat op het beeld. Vond je ooit een zo ontzettend dat je het voortdurend opnieuw wilde ? Bijvoorbeeld: Vond je ooit een boom zo ontzettend mooi dat je het voortdurend opnieuw wilde knuffelen?
+
+
+
+# Opgave 3:
+# Zoek je verhaal op die je bij lesbrief 1 hebt gemaakt. Maar vraag nu de naam van de hoofdpersoon aan de gebruiker!
+
+# Voorbeeldverhaal: Hoe heet je? Henk
+# Henk is een blije jongen. Hij gaat op de fiets naar school. Als Henk op school is gaat hij vrolijk naar de les. Einde!
+
+# Code die hier bij hoort:
+
+# naam = input("Hoe heet je?") 
+# print(naam + " is een blije jongen.") 
+# # denk aan de extra spatie! print("Hij gaat op de fiets naar school.") 
+# print("Als " + naam + " op school is gaat hij vrolijk naar de les.") 
+# print("Einde!")
+
+
+
+
+
+
+# Opgave 4
+# Schrijf een programma dat om drie woorden vraagt en vervolgens die drie woorden in alle zes mogelijke volgordes weer naar het scherm schrijft. Je moet zes keer een print commando geven!
+
+
+
+
+
+
+# Opgave 5
+# Schrijf een programma dat vraagt om vier getallen. Tel de eerste twee getallen bij elkaar op, deel dat dan door het derde getal, en vermenigvuldig het met het vierde getal. Print vervolgens het antwoord op het scherm.
+
+
+
+
+# Opgave 6
+# Schrijf een programma dat de gebruiker vraagt om vijf getallen en schrijf daarna het gemiddelde van die vijf getallen naar het scherm (niet afronden).
+
+
+
+
+# Opgave 7
+# Schrijf een programma dat de gebruiker vraagt om het huidige jaartal en om het jaar waarin hij geboren is. Reken uit hoe oud de gebruiker is aan het eind van het huidige jaar (in hele jaren) en schrijf het antwoord naar het scherm.
+
+
+Opgave 8
+# Bedenk wat de types zijn van de volgende tien dingen. Kies uit integer, float of string.
+# 19 “3” 21.234 “Hoela hoepen” 24 * 21.4 29 / 3 “293.3” 25.1 + 5 2 / 3 * 9 8 + 30 / 10
+# Zoals je hierboven kan zien, maakt het erg veel uit hoe je de berekening invoert. Voor de volgende opdracht ga je hier zelf een programma voor schrijven:
+
+
+
+# Opdracht 9
+# Schrijf een programma dat 2 of meer inputs vraagt, maak er een berekening mee en print het antwoord uit. In het programma moeten alle drie de datatypes voorkomen.
+# Als je even niet meer weet wat voor type iets heeft, bestaat er een functie die je hiervoor kan gebruiken. Als je namelijk type(variabele) gebruikt, zegt de computer wat het type is.
+
+# x = 8
+# print(type(x))
+# print(type("hallo"))
+# print(type(4.5))
+
+
+
+
